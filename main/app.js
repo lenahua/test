043d522bf511c7ff5a8eb17b3951c47e653663a3
jingwen <s888722@gmail.com>
2023-12-24 21:54:46 +0800

    test

---
+++ main/app.js
@@ -0,0 +1,11 @@
+// let header = document.querySelector("header");
+
+// window.addEventListener("scroll", () =>{
+//     if(window.pageYOffset != 0){
+//         header.style.backgroundColor = "white";
+//     } else{
+//         header.style ="";
+//     }
+// })
+
+//279 video
